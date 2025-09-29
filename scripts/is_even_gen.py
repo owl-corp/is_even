36a57e35ad56d84b4b732b72a0ee4856e33efa1c
Chris Lovering <chris.lovering.95@gmail.com>
2025-09-29 17:31:32 +0100

    Optimise the is_even logic

--- scripts/is_even_gen.py
+++ scripts/is_even_gen.py
@@ -1,12 +1,19 @@
+from random import shuffle
+
 from pathlib import Path
 
 with Path("is_even/is_even.py").open("w") as f:
-    f.write("import random\n\ndef is_even(num: int) -> bool:\n")
-    f.write("    if num == 0:\n        return True\n")
+    f.write("import ast\nimport random\n\n")
+    f.write("# No magic numbers\nTRUE='True'\nFALSE='False'\n\n")
+    f.write("INTERPRET = ast.literal_eval\n")
+    f.write("IS_EVEN_BOOL = bool\n\n\n")
+    f.write("def is_even(num: int) -> IS_EVEN_BOOL:\n")
+    nums = list(range(1_000_000))
+    shuffle(nums)
     f.writelines(
         [
-            f"    if num == {n}:\n        return {n % 2 == 0}\n"
-            for n in range(1, 1000000)
+            f"    if num == {n}:\n        return INTERPRET({'TRUE' if n % 2 == 0 else 'FALSE'})\n"
+            for n in nums
         ]
     )
     f.write("    return bool(random.getrandbits(1))\n")
