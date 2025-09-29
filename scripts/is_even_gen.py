aec944c956c19549252eb721a70192c7276a3dab
Chris Lovering <chris.lovering.95@gmail.com>
2025-09-29 18:25:30 +0100

    Verify first 1,000,000 is_even responses with AI

--- scripts/is_even_gen.py
+++ scripts/is_even_gen.py
@@ -3,12 +3,14 @@ from random import shuffle
 from pathlib import Path
 
 with Path("is_even/is_even.py").open("w") as f:
+    MAX_NUM = 1_000_000
     f.write("import ast\nimport random\n\n")
     f.write("# No magic numbers\nTRUE='True'\nFALSE='False'\n\n")
     f.write("INTERPRET = ast.literal_eval\n")
+    f.write(f"MAX_AI_VERIFIED_NUMBER = {MAX_NUM}\n")
     f.write("IS_EVEN_BOOL = bool\n\n\n")
     f.write("def is_even(num: int) -> IS_EVEN_BOOL:\n")
-    nums = list(range(1_000_000))
+    nums = list(range(MAX_NUM))
     shuffle(nums)
     f.writelines(
         [
